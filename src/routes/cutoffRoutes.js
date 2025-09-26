dc77641ef0ba66bebc844baa0fcbdd9ebd4a1449
GAURAV KATARA <gauravkatara53@gmail.com>
2025-09-26 12:16:21 +0530

    add

--- src/routes/cutoffRoutes.js
+++ src/routes/cutoffRoutes.js
@@ -4,6 +4,7 @@ import {
   updateCutoffController,
   deleteCutoffController,
   getAllCutoffsController,
+  deleteAllIIITCutoffsController,
 } from "../controllers/cutoffController.js";
 
 const router = Router();
@@ -12,5 +13,7 @@ router.post("/create/:slug", createCutoffController);
 router.put("/update/:slug", updateCutoffController);
 router.delete("/delete/:slug", deleteCutoffController);
 router.get("/all", getAllCutoffsController);
+
+router.delete("/iiit", deleteAllIIITCutoffsController);
 
 export default router;
